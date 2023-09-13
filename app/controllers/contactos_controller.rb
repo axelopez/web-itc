fe9b410fc5122e850e594c240e89a58d7b1c2128
axelopez <axelopez@gmail.com>
2023-09-12 23:23:14 -0600

    listado mensajes

--- app/controllers/contactos_controller.rb
+++ app/controllers/contactos_controller.rb
@@ -4,7 +4,7 @@ class ContactosController < ApplicationController
 
   # GET /contactos or /contactos.json
   def index
-    @contactos = Contacto.paginate(page: params[:page], per_page: 6)
+    @contactos = Contacto.paginate(page: params[:page], per_page: 3)
   end
 
   def confirmado
